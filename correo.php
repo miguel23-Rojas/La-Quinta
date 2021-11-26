f3f5dd03badcdb1e71fb86606acaac7b7cee0167
Miguel Rojas <rojashernandezmiguel237@gmail.com>
2021-11-25 19:05:33 -0600

    first commit

---
+++ correo.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['Enviar'])) {
+    if (!empty($_POST['name']) && !empty($_POST['tema']) 
+    && !empty($_POST['msg']) && !empty($_POST['email'])) {
+        $name = $_POST['name'];
+        $tema = $_POST['tema'];
+        $msg = $_POST['msg'];
+        $email = $_POST['email'];
+        $header = "From: noreply@example.com" . "\r\n";
+        $header = "Reply-To: noreply@example.com" . "\r\n";
+        $header = "X-Mailer: PHP/".phpversion();
+        $mail = @mail($name,$tema,$msg,$email);
+        if ($mail) {
+            echo "<h4>¡Mail enviado exitosamente!</h4>";
+        }
+    }
+}
