d9dfb7d9a58716194ee491f003dd0abaaf565a22
Miguel Rojas <rojashernandezmiguel237@gmail.com>
2021-12-06 21:03:40 -0600

    Detalles proveedores

--- correo2.php
+++ correo2.php
@@ -4,7 +4,7 @@ if (isset($_POST['Enviar'])) {
     if (!empty($_POST['name']) && !empty($_POST['Tlocal']) 
     && !empty($_POST['Telefono']) && !empty($_POST['Ciudad'])
     && !empty($_POST['interes']) && !empty($_POST['Comentarios'])
-    && !empty($_POST['Email'])
+    && !empty($_POST['email'])
     ) {
         $name = $_POST['name'];
         $Tlocal = $_POST['Tlocal'];
@@ -12,11 +12,20 @@ if (isset($_POST['Enviar'])) {
         $Ciudad = $_POST['Ciudad'];
         $interes = $_POST['interes'];
         $Comentarios = $_POST['Comentarios'];
-        $Email = $_POST['Email'];
+        $email = $_POST['email'];
         $header = "From: noreply@example.com" . "\r\n";
         $header = "Reply-To: noreply@example.com" . "\r\n";
         $header = "X-Mailer: PHP/".phpversion();
-        $mail = @mail($name,$Tlocal,$Telefono,$Ciudad,$interes,$Comentarios,$Email);
+
+        $mensaje = "Este mensaje fue enviado por " . $name . " \r\n";
+        $mensaje .= "Su e-mail es: " . $email . " \r\n";
+        $mensaje .= "Su tipo de local" . $Tlocal . " \r\n";
+        $mensaje .= "Su telefono " . $Telefono . " \r\n";
+        $mensaje .= "Cual es su interes" . $interes . " \r\n";
+        $mensaje .="Cual es su comentario". $Comentarios . " \r\n";
+        $mensaje .= "Enviado el " . date('d/m/Y', time());
+
+        $mail = @mail($email,$interes,utf8_decode($mensaje),$header);
         if ($mail) {
             echo "<h4>¡Mail enviado exitosamente!</h4>";
         }
